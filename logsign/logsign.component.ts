101cee3d1f4f0659bb4d7b4a7e60029080f063d9
Govind Singh <govind.resarch0000@rediffmail.com>
2019-05-26 01:02:49 +0530

    Init

---
+++ logsign/logsign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logsign',
+  templateUrl: './logsign.component.html',
+  styleUrls: ['./logsign.component.sass']
+})
+export class LogsignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
